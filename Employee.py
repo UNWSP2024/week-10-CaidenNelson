147fe62f5d5f5d65526d8511daa997e2c0e23438
CaidenNelson <cwnelson4@students.unwsp.edu>
2024-11-08 16:37:01 -0600

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -8,3 +8,19 @@
 # Mark Jones	39119	IT	Programmer
 # Joy Rogers	81774	Manufacturing	Engineer
 # The program should store the data in the three objects, then display the data for each employee on the screen.
+
+class Employees:
+    def __init__(self, name, ID_Number, Department, Job_Title):
+        self.name = name
+        self.ID_Number = ID_Number
+        self.Department = Department
+        self.Job_Title = Job_Title
+    def __str__(self):
+        return (f'{self.name} {self.ID_Number} {self.Department} {self.Job_Title}')
+
+susan = Employees('Susan Meyers','47899','Accounting','Vice President')
+mark = Employees('Mark Jones','39119','IT','Programmer')
+joy = Employees('Joy Rogers','81774','Manufacturing','Engineer')
+print(joy)
+print(susan)
+print(mark)
